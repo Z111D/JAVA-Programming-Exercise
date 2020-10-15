e56516ccb42b3a0b5fd42529f316fad2d62cc41a
Z111D <57184694+Z111D@users.noreply.github.com>
2020-10-15 10:36:20 +0800

    Add files via upload

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,17 @@
-public class HelloWorld {
-	public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
+class A
+{
+    int    x;
+    String s;
+};
+class HelloWorld
+{
+    public static void main(String[] args) 
+    {
+        A a= new A();
+        System.out.println(a.s);
+        System.out.println(a.x);
+	}
 }
+
+//null
+//0
